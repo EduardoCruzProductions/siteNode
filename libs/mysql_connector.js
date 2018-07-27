6b65af7fd99f6844982b7baf7dd3f4d418364b0a
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-07-27 14:21:51 -0300

    Reestruturando o conector

--- libs/mysql_connector.js
+++ libs/mysql_connector.js
@@ -1,14 +1,20 @@
 var mysql = require('mysql');
+
+var path = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "node_db"
+});
+
 var connection;
 
 module.exports = function() {
-
   if(!connection){
-    connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "12345"
-    });
+    connection = path.connect(function(err){
+      if(err) throw err;
+      console.log("Conectado!");
+    })
   }
 
   return connection;
