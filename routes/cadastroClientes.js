d7ed0203d4ed4004fffee5ef381cd83aa4246bc1
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-07-27 15:23:28 -0300

    Inciniando implementacao dos cadastros

--- routes/cadastroClientes.js
+++ routes/cadastroClientes.js
@@ -5,4 +5,11 @@ router.get('/', function(req, res, next) {
   res.render('cadastrocliente');
 });
 
+router.post('/add', function(req, res, next) {
+  var body = req.body;
+  console.log("Salvando forumlário...");
+  console.log(body);
+  res.redirect('/cadastroclientes');
+});
+
 module.exports = router;
