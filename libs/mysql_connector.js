22de9a5226c7ac285cb74b6ae8d818f7a1144e1c
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-08-31 17:04:29 -0300

    Inicio da implementação da classe DAO

--- libs/mysql_connector.js
+++ libs/mysql_connector.js
@@ -1,22 +1,10 @@
 var mysql = require('mysql');
 
-var path = mysql.createConnection({
+var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "12345",
   database: "node_db"
 });
 
-var connection;
-
-module.exports = function() {
-  if(!connection){
-    connection = path.connect(function(err){
-      if(err) throw err;
-      console.log("Conectado!");
-    })
-  }
-
-  return connection;
-
-}
+module.exports = connection;
--- routes/cadastroClientes.js
+++ routes/cadastroClientes.js
@@ -1,34 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "nodedb"
-});
+var clienteDao = require('../banco/ClienteDao');
 
 router.get('/', function(req, res, next) {
   res.render('cadastrocliente');
-  if(con === null){
-    con.connect(function(err) {
-      if (err) throw err;
-      console.log("Connected!");
-    });
-  }
 });
 
 router.post('/add', function(req, res, next) {
   var body = req.body;
   console.log("Salvando forumlário...");
   console.log(body);
-  var sql = "insert into cliente(nome,cpf,email) values ('"+body.nome+"','"+body.cpf+"','"+body.email+"')";
-  console.log(sql);
-  con.query(sql, function (err, result) {
-   if (err) throw err;
-   console.log("1 record inserted");
-  });
+  clienteDao.salvar(body);
 
   res.redirect('/cadastroclientes');
 });
